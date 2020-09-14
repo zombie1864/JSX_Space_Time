ca202e8ba1196a125c1f631d14af4e7adaea1054
jeff <zombie1864@gmail.com>
2020-09-14 13:08:37 -0400

    added donationsReducer to the entitiesReducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import usersReducer from "./users_reducer";
-import videosReducer from './videos_reducer'
+import videosReducer from './videos_reducer';
+import DonationsReducer from './donations_reducer'
 
 const entitiesReducer = combineReducers({
   users: usersReducer, 
-  videos: videosReducer
+  videos: videosReducer, 
+  donations: DonationsReducer, 
 });
 
 export default entitiesReducer;
