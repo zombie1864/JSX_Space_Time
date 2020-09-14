3107611308bfe68cffcf1ee449d918a81f2c31f6
jeff <zombie1864@gmail.com>
2020-09-14 11:29:55 -0400

    added donations action

--- frontend/actions/donation_action.js
+++ frontend/actions/donation_action.js
@@ -1,1 +1,11 @@
-import * as DonationAPIUtil from '../util/'
+import * as DonationAPIUtil from '../util/donation_api_util'; 
+export const RECEIVE_DONATION_REQ = 'RECEIVE_DONATION_REQ'; 
+
+const receiveDonationReq = donation => ({
+    type: RECEIVE_DONATION_REQ, 
+    donation, 
+})
+
+export const donationReq = () => dispatch => (
+    DonationAPIUtil.donationReq().then(donation => { dispatch(donationReq(donation))})
+); 
