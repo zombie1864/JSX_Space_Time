56e06225ae4d2cab55c1430606a00ac8c3ab92da
jeff <zombie1864@gmail.com>
2020-09-23 10:14:49 -0400

    finalized the donation show page

--- frontend/components/donations/donation_show.jsx
+++ frontend/components/donations/donation_show.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 class DonationShow extends React.Component {
     render() {
         return (
             <div>
-                <h1>THANKS</h1>
+                <div className = 'main-menu'>
+                    <header>
+                        <Link to = '/' className = 'signup-header-link'>
+                            <h1>
+                                <img className = 'home-icon' src={window.Home_icon} alt="HOME"/>
+                            </h1>
+                        </Link>
+                    </header> 
+                </div>
+
+                <img src={window.Thank_Card} alt="Thanks!" className = 'donation-thank-you-card'/>
             </div>
         )
     }
