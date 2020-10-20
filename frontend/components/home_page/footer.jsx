4ab0c9efe01b39e191116d263a4255c180bfdde7
jeff <zombie1864@gmail.com>
2020-10-20 16:04:56 -0400

    fixed the font to the footer if home page

--- frontend/components/home_page/footer.jsx
+++ frontend/components/home_page/footer.jsx
@@ -5,42 +5,42 @@ class Footer extends React.Component {
         return (
             <div>
                 <footer className="footer-field">
-                <ul className="footer-ul">
-                    <li className="">
-                    <a 
-                        href="https://zombie1864.github.io/" target = '_blank'
+                    <ul className="footer-ul">
+                        <li className="">
+                        <a 
+                            href="https://zombie1864.github.io/" target = '_blank'
+                                className = 'footer-link'>
+                            Portfolio Site
+                        </a>
+                        </li>
+                    </ul>
+                    <ul className="footer-ul">
+                        <li className="">
+                        <a
+                            href="https://www.linkedin.com/in/jeff-varela-a3194692/" target = '_blank'
+                                className = 'footer-link'
+                        >
+                            LinkedIn
+                        </a>
+                        </li>
+                    </ul>
+                    <ul className="footer-ul">
+                        <li className="">
+                        <a href="https://angel.co/u/jeff-varela" target = '_blank'
                             className = 'footer-link'>
-                        Portfolio Site
-                    </a>
-                    </li>
-                </ul>
-                <ul className="footer-ul">
-                    <li className="">
-                    <a
-                        href="https://www.linkedin.com/in/jeff-varela-a3194692/" target = '_blank'
-                            className = 'footer-link'
-                    >
-                        LinkedIn
-                    </a>
-                    </li>
-                </ul>
-                <ul className="footer-ul">
-                    <li className="">
-                    <a href="https://angel.co/u/jeff-varela" target = '_blank'
-                        className = 'footer-link'>
-                        AngelList
-                    </a>
-                    </li>
-                </ul>
-                <ul className="footer-ul">
-                    <li className="">
-                    <a href="https://github.com/zombie1864" target = '_blank'
-                        className = 'footer-link'>
-                        Github
-                    </a>
-                    </li>
-                </ul>
-                <aside className="footer-ul">© 2020 JSX Space Time</aside>
+                            AngelList
+                        </a>
+                        </li>
+                    </ul>
+                    <ul className="footer-ul">
+                        <li className="">
+                        <a href="https://github.com/zombie1864" target = '_blank'
+                            className = 'footer-link'>
+                            Github
+                        </a>
+                        </li>
+                    </ul>
+                    <aside className="footer-ul">© 2020 JSX Space Time</aside>
                 </footer>
             </div>
         )
