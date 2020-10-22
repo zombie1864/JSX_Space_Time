f7dc1e8949cc397787b186dfcc4f02225c795a94
jeff <zombie1864@gmail.com>
2020-10-22 14:45:19 -0400

    added video_text_container to the show page - making it in scale w window

--- frontend/components/videos/show_video.jsx
+++ frontend/components/videos/show_video.jsx
@@ -16,7 +16,7 @@ class ShowVideo extends React.Component {
             return null; // this is needed so that the DOM can read the code below, componentDidMount will then mount and the code below will execute 
         } 
         return (
-            <div>
+            <div className = 'video_show_page'>
                 <div className = 'main-menu'>
                     <header>
                     <Link to = '/' className = 'header-link'>
@@ -51,19 +51,21 @@ class ShowVideo extends React.Component {
                     </nav>
                 </div> 
                 <div className="video_text_container">
-                    <img src={video.image_url} width = '1280' height = '720' className = 'show-bck-img'/>
-                    <ReactPlayer
-                    className='react-player'
-                    url={video.video_url}
-                    width='100vh'
-                    height='50vh'
-                    />
-                    <p className = 'show-title' >
-                        {video.title}
-                    </p>
-                    <p className = 'show-description'>
-                        {video.description}
-                    </p>
+                    {/* <img src={video.image_url} width = '1280' height = '720' className = 'show-bck-img'/> */}
+                    <div className="video_text_container">
+                        <ReactPlayer
+                        className='react-player'
+                        url={video.video_url}
+                        width='100vh'
+                        height='50vh'
+                        />
+                        <p className = 'show-title' >
+                            {video.title}
+                        </p>
+                        <p className = 'show-description'>
+                            {video.description}
+                        </p>
+                    </div>
                 </div>               
             </div>
         )
