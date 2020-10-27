d2d784e3f893d9107e24b35c9df10d905bd500cb
jeff <zombie1864@gmail.com>
2020-10-27 16:47:16 -0400

    added the episodes unstylized

--- frontend/components/home_page_episodes/home_page_video_index.jsx
+++ frontend/components/home_page_episodes/home_page_video_index.jsx
@@ -4,7 +4,44 @@ class HomePageVideoIndex extends React.Component {
     render() {
         return (
             <div>
-                {/* <p> </p> */}
+                        const allVideos = this.props.videos.map(video => {
+            return (
+            <li 
+                key = { video.id } 
+                className = 'indexed-video'
+                >
+                <Link 
+                    to = {`/videos/${video.id}`}
+                    >
+                    <div 
+                        className = 'index_video_container'
+                        >
+                        <img 
+                            src={video.image_url} 
+                            width = '100%' 
+                            height = '100%'
+                            />
+                        <img 
+                            src={window.video_play_icon} alt="YES"
+                            className = 'video_play_icon'
+                            />
+                    </div>
+                </Link>
+                <p 
+                    className = 'video_details_title'
+                    >{video.title}</p>
+                <p 
+                    className = 'video_details'
+                    >{video.runtime}m</p>
+            </li>
+            )
+        })
+
+        return (
+            <div>
+                {allVideos}
+            </div>
+        )
             </div>
         )
     }
