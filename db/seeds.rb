6aedf3cae14f1d9bb4f1ba7e5201cf68c087ffe5
jeff <zombie1864@gmail.com>
2020-10-28 14:08:28 -0400

    added images to my assests/images dir

--- db/seeds.rb
+++ db/seeds.rb
@@ -45,19 +45,22 @@ video4 = Video.create!(
     title: 'How The Penrose Singularity Theorem Predicts The End of Space Time ',
     runtime: 16.58, 
     description: 'The Nobel prize in physics this year went to black holes. Generally speaking. Specifically, it was shared by the astronomers who revealed to us the Milky Way’s central black hole and by Roger Penrose, who proved that in general relativity, every black hole contains a place of infinite gravity - a singularity. But the true impact of Penrose’s singularity theorem would is much deeper  - it leads us to the limits Einstein’s great theory and to the origin of the universe.',
-    video_url: 'https://www.youtube.com/watch?v=z4odQd8q3xY&t=887s&ab_channel=PBSSpaceTime'
+    video_url: 'https://www.youtube.com/watch?v=z4odQd8q3xY&t=887s&ab_channel=PBSSpaceTime',
+    image_url: 'video4_img.jpg'
 )
 
 video5 = Video.create!(
     title: 'The Edge of an Infinite Universe',
     runtime: 18.29,
     description: 'Have you ever asked “what is beyond the edge of the universe?” And have you ever been told that an infinite universe that has no edge? You were told wrong. In a sense. We can define a boundary to an infinite universe, at least mathematically. And it turns out that boundary may be as real or even more real than the universe it contains.',
-    video_url: 'https://www.youtube.com/watch?v=tJevBNQsKtU&ab_channel=PBSSpaceTime'
+    video_url: 'https://www.youtube.com/watch?v=tJevBNQsKtU&ab_channel=PBSSpaceTime',
+    image_url: 'video5_img.jpg'
 )
 
 video6 = Video.create!(
     title: 'Why Quantum Computing Requires Quantum Cryptography', 
     runtime: 17.14, 
     description: 'Quantum theory may seem like an obscure subject of questionable relevance to the average person. But in fact much of our technological world depends on our understanding of the quantum properties of the subatomic universe. And soon, perhaps very soon, we’ll be interacting with the weirdness of quantum mechanics even more directly – with the coming of quantum computing and the quantum internet. Quantum computing is a topic that’s that has been well covered, so we’re going to be talking about the quantum internet. Specifically quantum cryptography and quantum key distribution – the foundations of the prospective quantum internet. We may come back to quantum computer in detail – but for now let me show you why their advent will demand a quantum internet. ',
-    video_url: 'https://www.youtube.com/watch?v=pi7YwxxZQ5A&ab_channel=PBSSpaceTime'
+    video_url: 'https://www.youtube.com/watch?v=pi7YwxxZQ5A&ab_channel=PBSSpaceTime',
+    image_url: 'video6_img.jpg'
 )
