18b52abfe0c622c9a50ef71254dbd815278b9b06
jeff <zombie1864@gmail.com>
2020-10-28 10:43:24 -0400

    stylized the videos, added the title episodes, and added the basic css file

--- frontend/components/home_page_episodes/home_page_video_index.jsx
+++ frontend/components/home_page_episodes/home_page_video_index.jsx
@@ -10,39 +10,42 @@ class HomePageVideoIndex extends React.Component {
     render() {
         const allVideos = this.props.videos.map(video => {
             return (
-            <li 
-                key = { video.id } 
-                className = 'indexed-video-ep'
-                >
-                <Link 
-                    to = {`/videos/${video.id}`}
+                <li 
+                    key = { video.id } 
+                    className = 'indexed-video'
                     >
-                    <div 
-                        className = 'index_video_container'
+                    <Link 
+                        to = {`/videos/${video.id}`}
                         >
-                        <img 
-                            src={video.image_url} 
-                            width = '100%' 
-                            height = '100%'
-                            />
-                        <img 
-                            src={window.video_play_icon} alt="YES"
-                            className = 'video_play_icon'
-                            />
-                    </div>
-                </Link>
-                <p 
-                    className = 'video_details_title'
-                    >{video.title}</p>
-                <p 
-                    className = 'video_details'
-                    >{video.runtime}min</p>
-            </li>
+                        <div 
+                            className = 'index_video_container'
+                            >
+                            <img 
+                                src={video.image_url} 
+                                width = '100%' 
+                                height = '100%'
+                                />
+                            <img 
+                                src={window.video_play_icon} alt="YES"
+                                className = 'video_play_icon'
+                                />
+                        </div>
+                    </Link>
+                    <p 
+                        className = 'video_details_title'
+                        >{video.title}</p>
+                    <p 
+                        className = 'video_details'
+                        >{video.runtime}min</p>
+                </li>
             )
         })
 
         return (
             <div>
+                <p className="episodes">
+                    episodes
+                </p>
                 {allVideos}
             </div>
         )
