d87fd81a902383937e95abe11b7b10ce62e43ead
jeff <zombie1864@gmail.com>
2020-11-09 14:28:02 -0500

    added the basics for the episode comp

---
+++ frontend/components/episodes/episodes.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class episodes extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>TEST</h1>
+            </div>
+        )
+    }
+}
+
+export default episodes
