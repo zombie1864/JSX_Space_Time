5a434adf8a42b1c4849c97412bf8da1a5d2909fc
jeff <zombie1864@gmail.com>
2020-11-12 10:27:58 -0500

    added the ep reducer

--- frontend/reducers/episodes_reducer.js
+++ frontend/reducers/episodes_reducer.js
@@ -1,1 +1,13 @@
 import RECEIVE_ALL_VIDEOS from '../actions/episode_action'
+
+const EpisodesReducer = ( state = {}, action ) => {
+    Object.freeze(state);
+    switch (action.type) {
+        case RECEIVE_ALL_VIDEOS:
+            return action.videos
+        default:
+            return state;
+    }
+};
+
+export default EpisodesReducer;
