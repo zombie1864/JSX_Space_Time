f6a6fbcc29acbf555f5d7f3737fd53eebd9748a5
jeff <zombie1864@gmail.com>
2020-11-12 10:30:30 -0500

    updated my entities reducer by adding ep reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -2,11 +2,14 @@ import { combineReducers } from "redux";
 import usersReducer from "./users_reducer";
 import videosReducer from './videos_reducer';
 import DonationsReducer from './donations_reducer'
+import EpisodesReducer from './episodes_reducer'
 
 const entitiesReducer = combineReducers({
   users: usersReducer, 
   videos: videosReducer, 
   donations: DonationsReducer, 
+  episodes: EpisodesReducer
+
 });
 
 export default entitiesReducer;
