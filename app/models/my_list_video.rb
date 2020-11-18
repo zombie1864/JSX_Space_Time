a5d9da3172a3056e72d579321a84d0b0e1f6fb39
jeff <zombie1864@gmail.com>
2020-11-18 17:33:34 -0500

    added video association to my_list_video

--- app/models/my_list_video.rb
+++ app/models/my_list_video.rb
@@ -2,5 +2,6 @@ class MyListVideo < ApplicationRecord
     validates :user_id, uniqueness: true 
     validates :video_id, uniqueness: true 
     belongs_to :my_list_videos_id, polymorphic: true 
+    belongs_to :video 
     belongs_to :user 
 end
